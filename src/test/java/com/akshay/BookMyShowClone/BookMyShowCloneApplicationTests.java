9489d48379990df46ead5382db09b333ef281485
Akshay <akshaybalan31@gmail.com>
2025-03-15 00:50:53 +0530

    initial commit

---
+++ src/test/java/com/akshay/BookMyShowClone/BookMyShowCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akshay.BookMyShowClone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookMyShowCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
